5b0e4e64b7811304a1dbcde28b1a0c583ab7cbdd
Christopher Waugh <kenkron@gmail.com>
2017-01-29 17:59:34 -0500

    cleaning & commenting

--- breathing.js
+++ breathing.js
@@ -3,40 +3,28 @@
 		//Input variables
 		$scope.compliance = 1;
 		$scope.resistance = 1;
-		
+
 		// variables
-		var timeConstant=1;
-		
-		var minHeight = 30;//min height of a lung
-		var maxHeight = 60;//max height of a lung
-		var currentHeight = minHeight;
-		//height of left lung for reference
-		var referenceHeight = minHeight;
-		var minWidth  = 30;
+		var timeConstant = 1;
+
+		var minLungHeight = 30; //min height of a lung
+		var maxLungHeight = 60; //max height of a lung
+		var currentHeight = minLungHeight; //height of right lung (influenced by constants)
+		var referenceHeight = minLungHeight; //height of left lung for reference (all constansts are 1)
+		var lungWidth = 30;
 		var thickness = 1;
-		var exhaled = 400; //height of lungs when exhaled (in pixels)
-		var leftLungDepth = 50; //how deep the breaths are (in pixels)
-		var leftLungFrequency = 20; //breaths per minute
-		var leftLungThickness = 8; //width of lung outline
 
 		var canvas = document.getElementById('canvas');
 		var ctx = canvas.getContext('2d');
 		var fps = 30;
 		var time = 0;
 
-		this.maxSide=function(){
-			return max(window.innerWidth(),window.innerHeight());
-		}
-
-		var drawEllipse = function(x, y, width, height) {
-			ctx.beginPath();
-			ctx.ellipse(x + width / 2, y + height / 2, width / 2, height / 2, 0, 0, 2 * Math.PI)
-			ctx.fill();
-			ctx.stroke();
+		this.maxSide = function() {
+			return max(window.innerWidth(), window.innerHeight());
 		}
 
 		var update = function() {
-			currentHeight= computeHeight(time, $scope.resistance, $scope.compliance);
+			currentHeight = computeHeight(time, $scope.resistance, $scope.compliance);
 			referenceHeight = computeHeight(time, 1, 1);
 			render();
 			time += 1.0 / fps;
@@ -44,25 +32,34 @@
 
 		/**updates the current height of the lungs based on
 		 * the time passed*/
-		var computeHeight = function(time, resistance, compliance){
-			timeConstant = resistance*compliance;
+		var computeHeight = function(time, resistance, compliance) {
+			timeConstant = resistance * compliance;
+			//This gets the time since the last exhale.
 			//5 time constants to fill lungs, means
 			//one breath every 10 time constants
-			time %= 10*timeConstant;
-			//position in inhale/exhale measured in time constants
-			var constants = (time%(5*timeConstant))/timeConstant;
-			//(0=exhaled, 1=inhaled):
-			var breath = Math.pow(0.63,constants);
+			time %= 10 * timeConstant;
+			//time constance since filling/emptying
+			var constants = (time % (5 * timeConstant)) / timeConstant;
+			//Amount exhaled (0=exhaled, 1=inhaled)
+			var breath = Math.pow(0.63, constants);
 			//Since fice time constants is "basically full",
 			//we need to scale to be totally full for smooth animation
-			breath = (breath-Math.pow(0.63,5))/(1-Math.pow(0.63,5));
-			//if inhaling:
-			if (time < 5*timeConstant){
-				breath = 1-breath;
+			breath = (breath - Math.pow(0.63, 5)) / (1 - Math.pow(0.63, 5));
+			//if inhaling, switch the breathing around:
+			if (time < 5 * timeConstant) {
+				breath = 1 - breath;
 			}
 			//smaller compliance=shallower breaths
-			breath *= compliance;
-			return breath*(maxHeight-minHeight)+minHeight;
+			breath = breath * compliance;
+			//convert into dimensions & return
+			return breath * (maxLungHeight - minLungHeight) + minLungHeight;
+		}
+
+		var drawEllipse = function(x, y, width, height) {
+			ctx.beginPath();
+			ctx.ellipse(x + width / 2, y + height / 2, width / 2, height / 2, 0, 0, 2 * Math.PI)
+			ctx.fill();
+			ctx.stroke();
 		}
 
 		var render = function() {
@@ -74,23 +71,23 @@
 			ctx.strokeStyle = 'black';
 			var xp = canvas.width * 0.01;
 			var yp = canvas.height * 0.01;
-			ctx.lineWidth = (thickness*xp)/$scope.compliance;
+			ctx.lineWidth = (thickness * xp) / $scope.compliance;
 			ctx.fillStyle = 'pink';
-			var xMargin = (50-minWidth)*xp/2;
-			var yMargin = (canvas.height-(minHeight+(maxHeight-minHeight)*2)*yp)/2;
-			drawEllipse(xMargin, yMargin, minWidth * xp, currentHeight * yp);
-			ctx.lineWidth = thickness*xp;
-			drawEllipse(50*xp+xMargin, yMargin, minWidth * xp, referenceHeight * yp);
+			var xMargin = (50 - lungWidth) * xp / 2;
+			var yMargin = (canvas.height - (minLungHeight + (maxLungHeight - minLungHeight) * 2) * yp) / 2;
+			drawEllipse(xMargin, yMargin, lungWidth * xp, currentHeight * yp);
+			ctx.lineWidth = thickness * xp;
+			drawEllipse(50 * xp + xMargin, yMargin, lungWidth * xp, referenceHeight * yp);
 			ctx.restore();
 		}
 
-		var updateCanvas = function(){
+		var updateCanvas = function() {
 			canvas.width = canvas.clientWidth;
 			canvas.height = canvas.clientHeight;
 		}
 
 		window.setInterval(update, 1 / fps);
-		window.addEventListener("resize",updateCanvas);
+		window.addEventListener("resize", updateCanvas);
 		updateCanvas();
 	}
 
